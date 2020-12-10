a189b0428993a678a6814f79ed1adf451d5ea579
robmccormack89 <robertmccormack147@gmail.com>
2020-12-10 13:34:46 +0000

    img alts

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -36,32 +36,32 @@ add_filter( 'woocommerce_register_post_type_product', 'nk_custom_post_type_label
 
 function nk_get_cpt_labels($single,$plural){
    $arr = array(
-      'name' => $plural,
-      'singular_name' => $single,
-      'menu_name' => $plural,
-      'add_new' => 'Add '.$single,
-      'add_new_item' => 'Add New '.$single,
-      'edit' => 'Edit',
-      'edit_item' => 'Edit '.$single,
-      'new_item' => 'New '.$single,
-      'view' => 'View '.$plural,
-      'view_item' => 'View '.$single,
-      'search_items' => 'Search '.$plural,
-      'not_found' => 'No '.$plural.' Found',
-      'not_found_in_trash' => 'No '.$plural.' Found in Trash',
-      'parent' => 'Parent '.$single
-   );
+    'name' => $plural,
+    'singular_name' => $single,
+    'menu_name' => $plural,
+    'add_new' => 'Add '.$single,
+    'add_new_item' => 'Add New '.$single,
+    'edit' => 'Edit',
+    'edit_item' => 'Edit '.$single,
+    'new_item' => 'New '.$single,
+    'view' => 'View '.$plural,
+    'view_item' => 'View '.$single,
+    'search_items' => 'Search '.$plural,
+    'not_found' => 'No '.$plural.' Found',
+    'not_found_in_trash' => 'No '.$plural.' Found in Trash',
+    'parent' => 'Parent '.$single
+ );
    return $arr;
 }
 
 /* add options page in backend via acf */;
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
-		'page_title' 	=> 'Site Settings',
-		'menu_title'	=> 'Site Settings',
-		'menu_slug' 	=> 'site-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
+  	'page_title' 	=> 'Site Settings',
+  	'menu_title'	=> 'Site Settings',
+  	'menu_slug' 	=> 'site-settings',
+  	'capability'	=> 'edit_posts',
+  	'redirect'		=> false
 	));
 }
 
@@ -696,25 +696,25 @@ require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
 
 function sixstar_theme_register_required_plugins()
 {
-    $plugins = array(
-        array(
-            'name' => 'Timber',
-            'slug' => 'timber-library',
-            'required' => true
-        )
-    );
-    $config  = array(
-        'id' => 'tgmpa', // Unique ID for hashing notices for multiple instances of TGMPA.
-        'default_path' => '', // Default absolute path to bundled plugins.
-        'menu' => 'tgmpa-install-plugins', // Menu slug.
-        'parent_slug' => 'themes.php', // Parent menu slug.
-        'capability' => 'edit_theme_options', // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-        'has_notices' => true, // Show admin notices or not.
-        'dismissable' => true, // If false, a user cannot dismiss the nag message.
-        'dismiss_msg' => '', // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false, // Automatically activate plugins after installation or not.
-        'message' => '' // Message to output right before the plugins table.
-    );
-    tgmpa($plugins, $config);
+  $plugins = array(
+    array(
+      'name' => 'Timber',
+      'slug' => 'timber-library',
+      'required' => true
+    )
+  );
+  $config  = array(
+    'id' => 'tgmpa', // Unique ID for hashing notices for multiple instances of TGMPA.
+    'default_path' => '', // Default absolute path to bundled plugins.
+    'menu' => 'tgmpa-install-plugins', // Menu slug.
+    'parent_slug' => 'themes.php', // Parent menu slug.
+    'capability' => 'edit_theme_options', // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+    'has_notices' => true, // Show admin notices or not.
+    'dismissable' => true, // If false, a user cannot dismiss the nag message.
+    'dismiss_msg' => '', // If 'dismissable' is false, this message will be output at top of nag.
+    'is_automatic' => false, // Automatically activate plugins after installation or not.
+    'message' => '' // Message to output right before the plugins table.
+  );
+  tgmpa($plugins, $config);
 }
 add_action('tgmpa_register', 'sixstar_theme_register_required_plugins');
