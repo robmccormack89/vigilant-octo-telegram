08df1b4e3e3c55c86d3f33798bbde8bc393c0528
robmccormack89 <robertmccormack147@gmail.com>
2021-02-24 19:45:37 +0000

    clickthru 2

--- woocommerce.php
+++ woocommerce.php
@@ -35,24 +35,22 @@ if ( is_singular( 'product' ) ) {
   
   Timber::render( 'single-product.twig', $context );
   
-} else {
+} else { // is not singular, then it must be an archive!
   
-  // get the main posts query
+  // get the main posts object via the standard wp archive query & assign as variable 'products'
   $posts = new Timber\PostQuery();
-  // set main query as products cariable 
   $context['products'] = $posts;
-  // main template for woo archives is woo-archive
-  $templates = array( 'shop.twig' );
-  // main tease template
+  
+  // define our archive & tease templates as arrays, which can be unshifted later depending on context
+  $templates = array('shop.twig');
   $tease_template = array('tease-product.twig');
-  // set the woo archive columns setting
+  $tease_term_template = array('tease-term.twig');
+  
+  // gets the woocommerce columns per row setting
   $context['products_grid_columns'] = wc_get_loop_prop('columns');
   
-  $term_slug = get_query_var('product_cat');
-  $context['the_term'] = get_term_by('slug', $term_slug, 'product_cat');
-  
   // if is list-view
-  if ( get_query_var('grid_list') == 'list-view' ) {
+  if (get_query_var('grid_list') == 'list-view') {
     // reset the woo archive columns setting
     $context['products_grid_columns'] = '1';
     // unshit the tease template variable with the new list tease template
@@ -60,95 +58,122 @@ if ( is_singular( 'product' ) ) {
     // then Restore the context and loop back to the main query loop.
     wp_reset_postdata();
   };
-  $context['tease_template'] = $tease_template; 
-
-  if ( is_product_category() ) {
-    
+  
+  // if is any new taxonomy, see is_tax wp dev handbook for details
+  if (is_tax('')) {
+    // get queried object stuff
     $queried_object = get_queried_object();
     $term_id = $queried_object->term_id;
+    $context['term_slug'] = $queried_object->slug;
     $context['term_id'] = $term_id;
+    // set the archive title
+    $context['title'] = single_term_title( '', false );
+    // get term thumbs
+    // get product category thumbnail
+    $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
+    $archive_header_bg = wp_get_attachment_url( $thumbnail_id );
+    if (!empty($archive_header_bg)) {
+      $context['archive_header_bg'] = $archive_header_bg;
+    } else {
+      $context['archive_header_bg'] = get_template_directory_uri() . '/assets/images/field.jpg';
+    };
+    // then Restore the context and loop back to the main query loop.
+    wp_reset_postdata();
+  };
+
+  // if is product category
+  if (is_product_category()) {
+    // get the category & set variable with get_term using the term id
+    $context['category'] = get_term( $term_id, 'product_cat' );
     // Get subcategories of the current category
     $context['term_subs'] = get_terms([
       'taxonomy'    => 'product_cat',
       'hide_empty'  => true,
       'parent'      => $term_id,
     ]);
-
-    // get the category & set variable with get_term using the term id
-    $context['category'] = get_term( $term_id, 'product_cat' );
-    // set the archive title
-    $context['title'] = single_term_title( '', false );
-    // get product category thumbnail
-    $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
-    $archive_header_bg = wp_get_attachment_url( $thumbnail_id );
-    if (!empty($archive_header_bg)) {
-      $context['archive_header_bg'] = $archive_header_bg;
-    } else {
-      $context['archive_header_bg'] = get_template_directory_uri() . '/assets/images/field.jpg';
-    }
+    // get term thumbs
+    foreach($context['term_subs'] as $item) {
+      
+      $thumb_id = get_term_meta($item->term_id, 'thumbnail_id', true);
+      $context['thumb_link'] = wp_get_attachment_url($thumb_id);
+      
+      if (current_product_series_var()) {
+        $url_out = get_term_link($item->term_id) . '?product_series=' . get_query_var('product_series');
+      } else {
+        $url_out = get_term_link($item->term_id);
+      };
+      $context['url_out'] = $url_out;
+    };
     // then Restore the context and loop back to the main query loop.
     wp_reset_postdata();
-    
   };
   
-  if ( is_tax('product_series') ) {
+  // is product_series archive
+  if (is_tax('product_series')) {
     
-    $queried_object = get_queried_object();
-    $term_id = $queried_object->term_id;
-    $context['term_slug'] = $queried_object->slug;
-    $context['term_id'] = $term_id;
     // Get subcategories of the current category
     $context['term_subs'] = get_terms([
       'taxonomy'    => 'product_series',
       'hide_empty'  => true,
       'parent'      => $term_id,
     ]);
-    
-    $context['new_terms'] = '';
-    
+    // get term thumbs
+    foreach($context['term_subs'] as $item) {
+      $thumb_id = get_term_meta($item->term_id, 'thumbnail_id', true);
+      $context['thumb_link'] = wp_get_attachment_url($thumb_id);
+      $context['url_out'] = get_term_link($item->term_id);
+    };
+    // set cats from series context as empty for now
+    $context['cats_from_series'] = '';
+    // if term subs is empty
     if (empty($context['term_subs'])) {
-      
-      $the_term_id = array();
+      // the cat ids for each product will be in an array
+      $the_term_ids = array();
+      // for each product
       foreach($context['products'] as $item) {
+        // get the cats for that product
         $product_cats = get_the_terms( $item->get_id(), 'product_cat' );
+        // for each product cat 
         foreach($product_cats as $product_cat) {
-          $the_term_id[] .= $product_cat->term_id;
+          // store cat id as a variable
+          $the_term_ids[] .= $product_cat->term_id;
         }
-        $sep = $the_term_id;
+        // we now have the cat ids for a product in the loop stored as an array, to be used in next step
+        $sep = $the_term_ids;
       };
-
-      $context['prod_ids'] = $sep;
-      $context['new_terms'] = get_terms([
+      // get the categories (parent only) related to products from the given series using the cat ids array we got above as we loop thru the products, then the cats
+      $context['cats_from_series'] = get_terms([
         'taxonomy'    => 'product_cat',
         'hide_empty'  => true,
+        'parent' => 0,
         'include' => $sep,
       ]);
-
-    }
-    
-    // set the archive title
-    $context['title'] = single_term_title( '', false );
-    // get product category thumbnail
-    $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
-    $archive_header_bg = wp_get_attachment_url( $thumbnail_id );
-    if (!empty($archive_header_bg)) {
-      $context['archive_header_bg'] = $archive_header_bg;
-    } else {
-      $context['archive_header_bg'] = get_template_directory_uri() . '/assets/images/field.jpg';
-    }
+      // get term thumbs
+      foreach($context['cats_from_series'] as $item) {
+        $thumb_id = get_term_meta($item->term_id, 'thumbnail_id', true);
+        $context['thumb_link'] = wp_get_attachment_url($thumb_id);
+        $context['url_out'] = get_term_link($item->term_id) . '?product_series=' . $context['term_slug'];
+      };
+      // unshit the tease term template variable with the new term template where term_subs is empty
+      array_unshift( $tease_term_template, 'tease-cat-from-series.twig' );
+    };
     // then Restore the context and loop back to the main query loop.
     wp_reset_postdata();
   };
   
   // if is main shop archive page
-  if ( is_shop() ) {
+  if (is_shop()) {
+    
     // set shop page archive title
     $context['title'] = 'Shop';
+    // get shop main thumbnail
     $context['archive_header_bg'] = get_template_directory_uri() . '/assets/images/field.jpg';
+    
     // then Restore the context and loop back to the main query loop.
     wp_reset_postdata();
   };
   
+  $context['tease_template'] = $tease_template; 
+  $context['tease_term_template'] = $tease_term_template; 
   Timber::render( $templates, $context );
-  
 }
